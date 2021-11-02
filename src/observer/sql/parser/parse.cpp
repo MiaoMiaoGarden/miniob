aba7fe3058e5b9f2dd472322631f5359275d4f3f
xiaolunzhou <51695571+JayL-zxl@users.noreply.github.com>
2021-11-02 21:38:25 +0800

    aaaa

--- src/observer/sql/parser/parse.cpp
+++ src/observer/sql/parser/parse.cpp
@@ -68,7 +68,14 @@ void value_init_string(Value *value, const char *v) {
 }
 void value_init_date(Value *value, const char *v) {
     value->type = DATES;
-    value->data = strdup(v);
+    char *date = strdup(v);
+    const char *delim = "-";
+    int year = atoi(strtok(date, delim));
+    int month = atoi(strtok(NULL, delim));
+    int day = atoi(strtok(NULL, delim));
+    int intdate = year * 10000 + month * 100 + day;
+    value->data = malloc(sizeof(intdate));
+    memcpy(value->data, &intdate, sizeof(intdate));
 }
 void value_destroy(Value *value) {
     value->type = UNDEFINED;
