99552ad80324d40fba00326f002bc6e01a934045
tuuuuuu <83738345+MiaoMiaoGarden@users.noreply.github.com>
2021-11-21 02:20:38 +0800

    nudl

--- src/observer/storage/common/table.cpp
+++ src/observer/storage/common/table.cpp
@@ -515,10 +515,10 @@ RC Table::scan_record(Trx *trx, ConditionFilter *filter, int limit, void *contex
         limit = INT_MAX;
     }
 
-    IndexScanner *index_scanner = find_index_for_scan(filter);
-    if (index_scanner != nullptr) {
-        return scan_record_by_index(trx, index_scanner, filter, limit, context, record_reader);
-    }
+    // IndexScanner *index_scanner = find_index_for_scan(filter);
+    // if (index_scanner != nullptr) {
+    //     return scan_record_by_index(trx, index_scanner, filter, limit, context, record_reader);
+    // }
 
     RC rc = RC::SUCCESS;
     RecordFileScanner scanner;
